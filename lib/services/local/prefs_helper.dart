df25e6b82bbb8c63bf107cb55ef7a78a5365aae9
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-20 01:24:50 +0300

    shared preferences helper

---
+++ lib/services/local/prefs_helper.dart
@@ -0,0 +1,20 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PrefsHelper {
+  static const _keyIsLoggedIn = 'isLoggedIn';
+
+  static Future<void> setLoggedIn(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_keyIsLoggedIn, value);
+  }
+
+  static Future<bool> isLoggedIn() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_keyIsLoggedIn) ?? false;
+  }
+
+  static Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+}
