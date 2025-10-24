58c87b0cc84dd399cb93ffaa8ca55c6e6e154ff9
Cyprians Mwanza <cypriansmwanza@outlook.com>
2025-10-24 09:24:58 +0300

    use of base url in AppConstants class

--- lib/data/ remote/api/retrofit/api_client.dart
+++ lib/data/ remote/api/retrofit/api_client.dart
@@ -1,19 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import '../../../../core/constants/app_constants.dart';
 import '../../models/api_note.dart';
 part 'api_client.g.dart';
 
-@RestApi(baseUrl: "https://jsonplaceholder.typicode.com")
+@RestApi(baseUrl: AppConstants.baseUrl)
 abstract class ApiClient {
   factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
   static ApiClient create() {
     final dio = Dio();
     dio.options = BaseOptions(
-      baseUrl: 'https://jsonplaceholder.typicode.com',
+      baseUrl: AppConstants.baseUrl,
       headers: {
         'Content-Type': 'application/json',
-        'User-Agent': 'FlutterApp', // Add this missing header
+        'User-Agent': 'FlutterApp',
       },
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
